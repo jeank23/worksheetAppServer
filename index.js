553ab1ce4a7b83b66c4f74884bf2dab1c8f9c5b5
JeanK <jean.salas@realself.com>
2019-06-16 11:46:05 -0600

    Last thing done `serializeUser and deserializeUser` things before the use of cookies when log in

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+
+mongoose.connect(keys.mongoURI,{ useNewUrlParser: true })
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
 const app = express();
 
-
-app.get('/', (req, res) => {
-    res.send({bye: 'buddy'});
-});
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
