5647456c9f8271b3f4851d4723ac7d16541d3f35
JeanK <jean.salas@realself.com>
2019-07-02 12:43:21 -0600

    Adding content

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID: '864684894670-dcmqf2sjhk7qag0slo4sv5hlesln40d5.apps.googleusercontent.com',
+    googleClientSecret: 'QDEGPw-E5sR2qIi_BX1UvlBR',
+    mongoURI: 'mongodb+srv://jeank23:lKzjEeFaTIa0MN8F@worksheetapp-iliul.mongodb.net/test?retryWrites=true&w=majority',
+    cookieKey: 'jhfgkejsbrfjhberkjbhkvakjehrbvLSDKUFHWWDCJHkjsdhbvz'
+}
