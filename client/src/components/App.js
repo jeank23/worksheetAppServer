f42c87cafc60e2fbd6636b4ec6e4a3d3bb63e76d
JeanK <jean.salas@realself.com>
2019-08-04 13:40:52 -0600

    Implementing user check when fall into the landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 
 const Landing = () => <div>Landing</div>;
 const Dashboard = () => <h3>Dashboard</h3>;
 
-const App = () => {
-    return (
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component={Landing} />
-                    <Route path="/dashboard" component={Dashboard} />
-                </div>
-            </BrowserRouter>   
-        </div>
-    );
+class App extends Component {
+
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div>
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component={Landing} />
+                        <Route path="/dashboard" component={Dashboard} />
+                    </div>
+                </BrowserRouter>   
+            </div>
+        );
+    }
 }
 
-export default App;
+export default connect(null, actions)(App);
