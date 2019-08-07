24ebe4afaa259a8dc1917c7ce032954daed7247c
JeanK <jean.salas@realself.com>
2019-08-06 19:39:30 -0600

    auth functionality implemented

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
-    return(
-        <nav>
-            <div className="nav-wrapper">
-            <a href="#" className="brand-logo">WorkSheetApp</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="/auth/google">Login</a></li>
-            </ul>
-            </div>
-        </nav>
-    );
+class Header extends Component {
+
+    renderContent(){
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
+    render(){
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                <Link to={ this.props.auth ? '/dashboard' : '/'} className="left brand-logo">WorkSheetApp</Link>
+                <ul id="nav-mobile" className="right">
+                    <li>
+                        {this.renderContent()}
+                    </li>
+                </ul>
+                </div>
+            </nav>
+        );
+    }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
