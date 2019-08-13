eb216245e1bb3244fb3fa68628020606de52574f
JeanK <jean.salas@realself.com>
2019-08-12 21:59:45 -0600

    First settings for the grid

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
 
-const columns = [
-    {key: 'id', name: 'ID'},
-    {key: 'title', name: 'Title'},
-    {key: 'count', name: 'Count'}
-];
 
-const rows = [
-    {id: 0, title: 'row1', count: 20}, 
-    {id: 1, title: 'row1', count: 40}, 
-    {id: 2, title: 'row1', count: 60}
-];
+const columns = () => {
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let cols = [];
 
-const Grid = () => {
-    return (
-        <ReactDataGrid
-        columns={columns}
-        rowGetter={i => rows[i]}
-        rowsCount={3}
-        minHeight={150} />
-    );
+    [...alphabet].forEach((letter, index) => {
+        cols[index] = {key: letter.toLowerCase(), name: letter};
+    });
+
+    return cols;
+};
+
+
+const rows = () => {
+    let alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let rows = [];
+    for(let i=0; i < 1000; i++){
+        rows[i] = {id: i};
+    }
+
+    return rows;
+};
+// const rows = [
+//     {id: 0, title: 'row1', count: 20}, 
+//     {id: 1, title: 'row2', count: 40}, 
+//     {id: 2, title: 'row3', count: 60}
+// ];
+
+class Grid extends Component {
+    render(){
+        console.log(columns());
+        return (
+            <ReactDataGrid
+            columns={columns()}
+            rowGetter={i => rows()[i]}
+            rowsCount={100} />
+        );
+    }
 }
 
 export default Grid;
